8952212b7d823ccfeffe81ad10ded626eac8d7d5
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2023-07-02 11:53:44 +0200

    add translate

--- index.js
+++ index.js
@@ -1,61 +1,55 @@
-
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-// import NewsApiService from './new-api-service';
-
 const refs = {
-    timerContainer: document.querySelector('.timer'),
-    
-    fieldEl: document.querySelector('.field'),
-    daysEl: document.querySelector('.days'),
-    hoursEl: document.querySelector('.hours'),
-    minutesEl: document.querySelector('.minutes'),
-    secondsEl: document.querySelector('.seconds'),
-    giftBtn: document.querySelector('.btn'),
-} 
+  timerContainer: document.querySelector(".timer"),
+  fieldEl: document.querySelector(".field"),
+  daysEl: document.querySelector(".days"),
+  hoursEl: document.querySelector(".hours"),
+  minutesEl: document.querySelector(".minutes"),
+  secondsEl: document.querySelector(".seconds"),
+  giftBtn: document.querySelector(".btn"),
+};
 
 const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
 const day = hour * 24;
 
-refs.giftBtn.addEventListener('click', onStart);
+refs.giftBtn.addEventListener("click", onStart);
 
 function onStart() {
-     let idInterval = null;
-        idInterval = setInterval(() => {
-            const newYear = new Date('Jan 1 2023 00:00:00')     
-            // console.log(newYear)    
-            const startTime = Date.now()
-            // console.log(startTime)
-        let lefUntil = newYear - startTime;
-        const timeOnDisplay = convertMs(lefUntil);
-                                                
-        refs.daysEl.textContent = addLeadingZero(timeOnDisplay.days);
-        refs.hoursEl.textContent = addLeadingZero(timeOnDisplay.hours);
-        refs.minutesEl.textContent = addLeadingZero(timeOnDisplay.minutes);
-        refs.secondsEl.textContent = addLeadingZero(timeOnDisplay.seconds);
-       
-        let endTime = refs.daysEl.textContent + refs.hoursEl.textContent + refs.minutesEl.textContent + refs.secondsEl.textContent;
-                //  console.log(endTime)   
+  let idInterval = null;
+  idInterval = setInterval(() => {
+    const newYear = new Date("Jan 1 2024 00:00:00");
+    const startTime = Date.now();
+    let lefUntil = newYear - startTime;
+    const timeOnDisplay = convertMs(lefUntil);
 
-        if (endTime === '00000000'){
-        clearInterval(idInterval);
-        alert ('🎄 З НОВИМ РОКОМ!!! ГОЙДА ДІСТАВАТИ ПОДАРУНКИ!!!');
-        return
-        }
+    refs.daysEl.textContent = addLeadingZero(timeOnDisplay.days);
+    refs.hoursEl.textContent = addLeadingZero(timeOnDisplay.hours);
+    refs.minutesEl.textContent = addLeadingZero(timeOnDisplay.minutes);
+    refs.secondsEl.textContent = addLeadingZero(timeOnDisplay.seconds);
 
-    }, 1000);
-};
+    let endTime =
+      refs.daysEl.textContent +
+      refs.hoursEl.textContent +
+      refs.minutesEl.textContent +
+      refs.secondsEl.textContent;
+
+    if (endTime === "00000000") {
+      clearInterval(idInterval);
+      alert("🎄 Happy New Year!!! Let`s enjoy giving and receiving gifts!!!");
+      return;
+    }
+  }, 1000);
+}
 
 function convertMs(lefUntil) {
-    const days = Math.floor(lefUntil / day);
-    const hours = Math.floor((lefUntil % day) / hour);
-    const minutes = Math.floor(((lefUntil % day) % hour) / minute);
-    const seconds = Math.floor((((lefUntil % day) % hour) % minute) / second);
-    return { days, hours, minutes, seconds };
-};
-    
+  const days = Math.floor(lefUntil / day);
+  const hours = Math.floor((lefUntil % day) / hour);
+  const minutes = Math.floor(((lefUntil % day) % hour) / minute);
+  const seconds = Math.floor((((lefUntil % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
+
 function addLeadingZero(value) {
-    return value.toString().padStart(2, "0")
-};
+  return value.toString().padStart(2, "0");
+}
